a786a592bd6b8074d484cb16b0b5805fdce591f2
mdlawrenceusa <mdlawrence@me.com>
2022-03-03 17:56:05 -0500

    4 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,14 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared with ET-720!');
   }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+
+
+
+
 }
 
 
